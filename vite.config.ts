e1deaded4fcaac5b824db1439b2300fa2f684597
The Trovira Company <thetroviracompany@gmail.com>
2026-02-07 00:41:01 +0530

    Update project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/trovira/',   // GitHub repo name
+  base: '/', // REQUIRED for custom domain
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 1500
