5751c85ef6acb81f67971b8f4c041dd38ac0d13e
The Trovira Company <thetroviracompany@gmail.com>
2026-02-07 00:27:52 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base: '/', // default base for non-GitHub deployments
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1500
-  }
+  base: '/trovira/',
 });
